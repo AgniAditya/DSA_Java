1c7b1e2bf80bd86a52cab812a4b540979d03052c
AgniAditya <agniaditya05@gmail.com>
2025-03-25 15:27:42 +0530

    Final hashmap

--- src/hashmap/Main.java
+++ src/hashmap/Main.java
@@ -46,10 +46,19 @@ public class Main {
         // map.put("Agni", 20);
         // System.out.println(map.get("Aditya"));
 
-        MapUsingHash map = new MapUsingHash();
-        map.put("BMW", "Car");
-        map.put("Tom", "Cartoon");
-        map.put("Mumbai", "City");
-        System.out.println(map.get("BMW"));
+        // MapUsingHash map = new MapUsingHash();
+        // map.put("BMW", "Car");
+        // map.put("Tom", "Cartoon");
+        // map.put("Mumbai", "City");
+        // System.out.println(map.get("Tom"));
+
+        HashMapFinal<String,Integer> map = new HashMapFinal<>();
+        map.put("Aditya",1);
+        map.put("Adi",2);
+        map.put("agni",3);
+        System.out.println(map);
+        map.remove("Adi");
+        System.out.println(map);
+        System.out.println(map.containKey("Adi"));
     }
 }
---
+++ src/hashmap/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+// HashMap<String,Integer> map = new HashMap<>();
+        // map.put("Aditya", 10);
+        // map.put("Adi", 15);
+        // map.put("Agni", 20);
+        // System.out.println(map.get("Aditya"));
