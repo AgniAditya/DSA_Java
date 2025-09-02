f18397d8e62cd2f01e6f1b15d2332dd4fa979eb5
AgniAditya <agniaditya05@gmail.com>
2025-09-02 22:10:45 +0530

    Add implementation for A. Sum of Round Numbers problem

---
+++ CodeForces/A_SumOfRoundNumbers.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class A_SumOfRoundNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfTestCases = sc.nextInt();
+
+        while (numOfTestCases-- > 0) {
+            ArrayList<Integer> roundedNumbers = new ArrayList<>();
+            int number = sc.nextInt();
+            int place = 1;
+            while (number > 0) {
+                int rem = number % 10;
+                if(rem != 0) roundedNumbers.add(rem * place);
+                number /= 10;
+                place *= 10;
+            }
+            
+            System.out.println(roundedNumbers.size());
+            for(int i : roundedNumbers) System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
